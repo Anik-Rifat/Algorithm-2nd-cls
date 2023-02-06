630ee1760dbfdc0789fa81ac85c7a08c4c9992b7
Md. Anik Ahmed Rifat <96744824+Anik-Rifat@users.noreply.github.com>
2023-02-06 21:45:31 +0600

    Add files via upload

---
+++ pointers.c
@@ -0,0 +1,92 @@
+// <!-----1- intro pointers 
+
+#include<stdio.h>
+// // int main()
+// // {
+// //   int a=10.5;
+// //     int *ptr;
+// //     ptr = &a;
+// //     *ptr = 9;
+    
+// //     printf("%d", *ptr+1);
+    
+    
+// //   return 0; 
+// // }
+
+// // <!-----2--- pointers
+
+
+#include<stdio.h>
+// // int main()
+// // {
+// //     int x, y, temp;
+// //  x=45;
+// //  y=25;
+// //  printf("Before swap: x=%d\n", x,y);
+// //  temp =x;
+ 
+// //   x=y;
+// //   y=temp;
+
+// //     printf("After swap x= %d", x,y);
+    
+    
+// //   return 0; 
+// // }
+
+
+// // 3................... Swap
+
+#include<stdio.h>
+// // void swap (int *x, int *y){
+// //     int temp;
+// //     temp = *x;
+// //     *x=*y;
+// //     *y=temp;
+// // }
+// // int main()
+// // {
+// //     int x, y;
+// //  x=45;
+// //  y=25;
+// //  printf("Before swap: x=%d,y= %d\n",  x,y);
+
+ 
+// //  swap(&x,&y);
+// //     printf("After swap x= %d, y=%d",  x,y);
+    
+    
+// //   return 0; 
+// // }
+
+
+
+
+// // 4............ array & pointers
+
+
+#include<stdio.h>
+// void display_array_elements(int n, int *ptr){
+//     for(int i =0; i<n; i++){
+//         printf("%d ", *(ptr+i) );
+//     }   
+// }
+
+
+// int main(){
+//     int my_array[]={10,20,33,44,55};
+
+//     int n=sizeof(my_array)/sizeof(my_array[0]);
+//     display_array_elements(n, my_array);
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
